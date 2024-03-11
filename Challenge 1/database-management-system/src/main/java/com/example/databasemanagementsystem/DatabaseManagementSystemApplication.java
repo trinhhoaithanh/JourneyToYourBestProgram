2c36e3c6d1abe1b96f85704b3ef86c9e50268293
ththanh <hoaithanhtrinh2002@gmail.com>
2024-03-11 11:45:25 +0700

    initialize challenge 1

---
+++ Challenge 1/database-management-system/src/main/java/com/example/databasemanagementsystem/DatabaseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.databasemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Challenge 1/database-management-system/src/test/java/com/example/databasemanagementsystem/DatabaseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databasemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
